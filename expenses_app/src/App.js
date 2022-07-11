7ba046209fc7d920e229218fbe8a3e65f75281cb
wmonsalv <55903739+wmonsalv@users.noreply.github.com>
2022-07-11 09:15:01 -0400

    setting up new expense

--- expenses_app/src/App.js
+++ expenses_app/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import Expenses from "./components/Expenses"
+import Expenses from "./components/Expenses/Expenses"
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   const expenses = [
@@ -27,6 +28,7 @@ function App() {
 
   return (
     <div className="App">
+    <NewExpense/>
     <Expenses items={expenses}/>
     </div>
   );
--- expenses_app/src/components/Expenses/ExpenseItem.js
+++ expenses_app/src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,24 @@
+import React, { useState } from "react"
 import "./ExpenseItems.css"
 import ExpenseDate from "./ExpenseDate"
 
+
 function ExpenseItem(props) {
+
+    const [title, setTitle] = useState(props.title);
+
+    const changeMyTitle = () => {
+        setTitle('Updated')
+    }
 
     return (
         <div className="expense-item">
             <ExpenseDate date={props.date}/>
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
+            <button onClick={changeMyTitle}>Change Title</button>
         </div>
     );
 }
