a212da2dc653b4e069529901e7d12c13e09b3b74
wmonsalv <55903739+wmonsalv@users.noreply.github.com>
2022-07-23 21:17:47 -0400

    Update ChartBar.js

--- expenses_app/src/components/Charts/ChartBar.js
+++ expenses_app/src/components/Charts/ChartBar.js
@@ -10,11 +10,12 @@ const ChartBar = (props) => {
 
     return (
         <div className="chart-bar">
-            <div className="chartBar__inner">
-                <div 
-                className="chart-bar__fill" style={{height: barFillHeight}}></div>
+            <div className="chart-bar__inner">
+                <div className="chart-bar__fill" 
+                style={{height:barFillHeight}}
+                ></div>
             </div>
-            <div className="chart-bar__label">{props.label}</div>
+            <div className="chart-bar_label">{props.label}</div>
         </div>
     )
 
